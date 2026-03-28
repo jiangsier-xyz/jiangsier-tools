d54b0a1a2fc6dec4c6f6f414d351d3dee28b1ca6
leiyi.jp <leiyi.jp@alibaba-inc.com>
2026-03-28 21:02:02 +0800

    feat: add installation script for CUDA 12.9 and driver 575.57.08

---
+++ driver/install-cu129.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# settings
+DRIVER_VERSION="575.57.08"
+CUDA_VERSION="12.9.1"
+CUDNN_VERSION="9.16.0.29"
+IS_INSTALL_eRDMA="FALSE"
+IS_INSTALL_RDMA="FALSE"
+INSTALL_DIR="/root/auto_install"
+
+# using .run to install driver and cuda
+auto_install_script="auto_install_v4.0.sh"
+
+script_download_url=$(curl http://100.100.100.200/latest/meta-data/source-address | head -1)"/opsx/ecs/linux/binary/script/${auto_install_script}"
+echo $script_download_url
+
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR && wget -t 10 --timeout=10 $script_download_url && bash ${INSTALL_DIR}/${auto_install_script} $DRIVER_VERSION $CUDA_VERSION $CUDNN_VERSION $IS_INSTALL_RDMA $IS_INSTALL_eRDMA
